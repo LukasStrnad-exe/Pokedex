33603cb047527eef412297f51a6792dc0b465c50
Lukas Strnad <lukas.strnad222@gmail.com>
2024-11-14 22:50:30 +0100

    added_styled_loop_PokemonCard

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const PokeDex = [];
 
 async function onload() {
     await loopPushData();
-    output();
+    render();
 }
 
 async function loopPushData() {
-    for (let i = 1; i < 21; i++) {
+    for (let i = 650; i < 700; i++) {
         await pushData(i)
     } 
 }
@@ -16,8 +16,29 @@ async function pushData(path = "") {
     let response = await fetch(base_url + path);
     let responseToJson = await response.json();
     PokeDex.push(responseToJson);
-  }
+}
 
-function output() {
-    console.log(PokeDex);
+function render() {
+    for (let i = 0; i < PokeDex.length; i++) {
+        const pokemon = PokeDex[i];
+        let container = document.getElementById('container');
+        container.innerHTML += containerTemplate(pokemon,i);
+        BgColorType(pokemon,i)
+    }
+}
+
+function displaytype1(pokemon){
+    let type1 = pokemon.types[1] ? pokemon.types[1].type.name : '';
+    return type1;
+}
+
+function deletetype1(pokemon, i) {
+    let type1 = pokemon.types[1] ? pokemon.types[1].type.name : '';
+    if (type1 === "") {
+        return ``;
+    } else {
+        return `
+            <div class="pokemon_1_element" id="type1${i}">${type1}</div>
+        `;
+    }
 }
---
+++ scripts/template.js
@@ -0,0 +1,24 @@
+function containerTemplate(pokemon,i){
+    let sprite = pokemon.sprites.front_default;
+    let id = pokemon.id;
+    let name = pokemon.name;
+    let type0 = pokemon.types[0].type.name;
+    return `
+        <div class="pokemon_card">
+            <div class="pokemon_img">
+                <img src="${sprite}" alt="" />
+            </div>
+            <div class="pokemon_nr">
+                <p>${id}#</p>
+            </div>
+            <div class="pokemon_name">
+                <h3>${name}</h3>
+            </div>
+            <div class="pokemon_elements">
+                <div class="pokemon_0_element" id="type0${i}">${type0}</div>
+                ${deletetype1(pokemon,i)}
+            </div>
+        </div>
+        `;
+}
+
